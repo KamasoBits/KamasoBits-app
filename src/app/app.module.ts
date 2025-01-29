411f06725b2b5ef50a742fa5f01996738840f61e
KamasoBits <kitkatkamaso@gmail.com>
2025-01-29 22:00:26 +0300

    Changes on header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
